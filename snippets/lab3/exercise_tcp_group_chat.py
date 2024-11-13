aa2f0ef71f651172fd09ca18ddaba90cc1061f71
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-11-13 18:09:24 +0100

    Implement observer pattern

--- snippets/lab3/exercise_tcp_group_chat.py
+++ snippets/lab3/exercise_tcp_group_chat.py
@@ -1,46 +1,67 @@
 import sys
+from typing import List, Tuple
 from snippets.lab2 import address, local_ips, message
 from snippets.lab3 import Client, Connection, Server
 
 
-clients = [Client(address(peer)) for peer in sys.argv[2:]]
+class TcpPeer:
+    def __init__(self, port: int, peers: List[Tuple[str, int]], on_new_connection, on_peer_death):
+        self.__clients = [Client(peer) for peer in peers]
+        self.__server = Server(port, on_new_connection)
+        self.__on_peer_death = on_peer_death
 
-port = int(sys.argv[1])
+    def send_all(self, message):
+        died_peers = []
+        for client in self.__clients:
+            try:
+                client.send(message)
+            except BrokenPipeError:
+                died_peers.append(client)
+        for died_peer in died_peers:
+            self.__on_peer_death(died_peer)
+            self.__clients.remove(died_peer)
+        
 
-def on_message_received(event, payload, connection: Connection, error):
-    match event:
-        case 'message':
-            print(payload)
-        case 'close':
-            print(f"Connection with peer {connection.remote_address} closed")
-            global remote_peer; remote_peer = None
-        case 'error':
-            print(error)
-
-def on_new_connection(event, connection, address, error):
-    match event:
-        case 'listen':
-            print(f"Server listening on port {address[0]} at {', '.join(local_ips())}")
-        case 'connect':
-            print(f"Open ingoing connection from: {address}")
-            connection.callback = on_message_received
-            global remote_peer; remote_peer = connection
-        case 'stop':
-            print(f"Stop listening for new connections")
-        case 'error':
-            print(error)
-
-server = Server(port, on_new_connection)
-
-username = input('Enter your username to start the chat:\n')
-print('Type your message and press Enter to send it. Messages from other peers will be displayed below.')
-while True:
-    try:
-        content = input()
-        for client in clients:
-            client.send(message(text=content, sender=username))
-    except (EOFError, KeyboardInterrupt):
-        for client in clients:
+    def close(self):
+        for client in self.__clients:
             client.close()
-        server.close()
-        break
+        self.__server.close()
+        
+
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    peers_addresses = [address(peer) for peer in sys.argv[2:]]
+
+    def on_message_received(event, payload, connection: Connection, error):
+        match event:
+            case 'message':
+                print(payload)
+            case 'error':
+                print(error)
+
+    def on_new_connection(event, connection, address, error):
+        match event:
+            case 'listen':
+                print(f"Server listening on port {address[0]} at {', '.join(local_ips())}")
+            case 'connect':
+                print(f"Open ingoing connection from: {address}")
+                connection.callback = on_message_received
+            case 'stop':
+                print(f"Stop listening for new connections")
+            case 'error':
+                print(error)
+
+    def on_peer_death(client: Client):
+        print(f"Connection with peer {client.remote_address} closed")
+
+    username = input('Enter your username to start the chat:\n')
+    print('Type your message and press Enter to send it. Messages from other peers will be displayed below.')
+    peer = TcpPeer(port, peers_addresses, on_new_connection, on_peer_death)
+
+    while True:
+        try:
+            content = input()
+            peer.send_all(message(content, username))
+        except (EOFError, KeyboardInterrupt):
+            peer.close()
+            break
